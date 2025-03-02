b4a477e042177b5abafc54ce5a4b464cdcb5a2cb
QuaidWilk02 <Wilkinsq@msu.edu>
2025-03-01 23:57:01 -0500

    Update InputManager.cs

--- Assets/TutorialInfo/Scripts/InputManager.cs
+++ Assets/TutorialInfo/Scripts/InputManager.cs
@@ -1,8 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     private InputSystem_Actions acting;
 
+    private void Awake()
+    {
+        acting = new InputSystem_Actions();
+    }
 
+    private void OnEnable()
+    {
+        acting.Enable();
+    }
+
+    private void OnDisable()
+    {
+        acting.Disable();
+    }
+
+    private void Start()
+    {
+        acting.UI.Click.started += ctx => StartTouch(ctx);
+        acting.UI.Click.canceled += ctx => EndTouch(ctx);
+    }
+
+    private void StartTouch(InputAction.CallbackContext context)
+    {
+
+    }
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+
+    }
 }
