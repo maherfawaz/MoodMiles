2f0327b9c1f888902f220eb6ce6c35c4ef8c80db
QuaidWilk02 <Wilkinsq@msu.edu>
2025-03-15 13:23:34 -0400

    Bear Attack

---
+++ Assets/Paw.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Paw : MonoBehaviour
+{
+    public float QuickTime;
+    public float ptime;
+    public float attacks;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        QuickTime -= Time.deltaTime;
+        if (QuickTime <= 0)
+        {
+            QuickTime = ptime;
+            attacks = 0;
+        }
+        if(attacks == 10)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
