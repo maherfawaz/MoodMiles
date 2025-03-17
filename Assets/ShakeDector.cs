e1abe97d6f0f0fd56b57b9a3beb5edbd4acc106e
Maher Fawaz <maherfawaz@outlook.com>
2025-03-17 10:52:31 -0400

    Update ShakeDector.cs

--- Assets/ShakeDector.cs
+++ Assets/ShakeDector.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 [RequireComponent(typeof(dogManager))]
+
 public class ShakeDector : MonoBehaviour
 {
     public float ShakeDetectionThreshold;
     public float MinShakeInterval;
+
+    private Vector3 accel;
+    private bool permissionGranted = false;
 
     private float sqrShakeDetectionThreshold;
     private float timeSinceLastShake;
@@ -14,15 +19,56 @@ public class ShakeDector : MonoBehaviour
     {
         sqrShakeDetectionThreshold = Mathf.Pow(ShakeDetectionThreshold, 2);
         DogManager = GetComponent<dogManager>();
+
+        if (Application.isEditor) {
+            Debug.Log("Running in Editor");
+            return;
+        }
+
+        RequestPermission();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.acceleration.sqrMagnitude >= sqrShakeDetectionThreshold && Time.unscaledTime >= timeSinceLastShake + MinShakeInterval)
+        if (Application.isEditor || !permissionGranted) {
+            return;
+        }
+
+        if (Accelerometer.current == null) {
+            Debug.Log("Accelerometer not available");
+            return;
+        }
+
+        accel = Accelerometer.current.acceleration.ReadValue();
+        
+        if (accel.sqrMagnitude >= sqrShakeDetectionThreshold && Time.unscaledTime >= timeSinceLastShake + MinShakeInterval)
         {
-            DogManager.ShakeRigidbodies(Input.acceleration);
+            DogManager.ShakeRigidbodies(accel);
             timeSinceLastShake = Time.unscaledTime;
+        }
+    }
+
+    async void RequestPermission() {
+#if UNITY_EDITOR
+        Debug.Log("Editor Platform");
+#endif
+#if UNITY_ANDROID
+        AndroidRuntimePermissions.Permission result = await AndroidRuntimePermissions.RequestPermissionAsync("android.permission.ACTIVITY_RECOGNITION");
+        if (result == AndroidRuntimePermissions.Permission.Granted) {
+            permissionGranted = true;
+            Debug.Log("Permission granted");
+            InputSystem.EnableDevice(Accelerometer.current);
+        } else {
+            Debug.Log("Permission denied");
+        }
+#endif
+    }
+
+    void OnApplicationPause(bool pause) {
+        if (!pause && permissionGranted) {
+            // Reinitialize the accelerometer when the app is resumed
+            InputSystem.EnableDevice(Accelerometer.current);
         }
     }
 }
