d86ae62ee6a6daf5e7700168aa516924fb9cc899
QuaidWilk02 <Wilkinsq@msu.edu>
2025-03-30 21:06:01 -0400

    Sign Up

--- Assets/Quaid/StaticHp.cs
+++ Assets/Quaid/StaticHp.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class StaticHp : MonoBehaviour
 {
-    public static int totalHP = 8;
+    public static int totalHP = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
