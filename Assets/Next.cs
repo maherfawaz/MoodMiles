c4bf72175bbebb3bf2b40f26edc0855b938b2671
Maher Fawaz <maherfawaz@outlook.com>
2025-04-10 00:57:28 -0400

    Update Next.cs

--- Assets/Next.cs
+++ Assets/Next.cs
@@ -11,7 +11,7 @@ public class Next : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene("Quaid Base");
         }
     }
 }
