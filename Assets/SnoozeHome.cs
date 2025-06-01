9e0fdb1b461f01c6f69f107832e379c5ffc5ff19
QuaidWilk02 <Wilkinsq@msu.edu>
2025-05-31 21:06:11 -0400

    UI home for mission fix

--- Assets/SnoozeHome.cs
+++ Assets/SnoozeHome.cs
@@ -4,6 +4,8 @@ public class SnoozeHome : MonoBehaviour
 {
     public GameObject mis;
     public GameObject prog;
+    public GameObject count;
+    public GameObject main;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,11 +20,19 @@ public class SnoozeHome : MonoBehaviour
         {
             mis.SetActive(true);
             prog.SetActive(false);
+            count.SetActive(false);
+            main.GetComponent<Camera>().backgroundColor = Color.cyan;
         }
         if (Snooze.progress == true)
         {
             mis.SetActive(false);
             prog.SetActive(true);
+            count.SetActive(true);
+            main.GetComponent<Camera>().backgroundColor = Color.black;
+        }
+        else
+        {
+            main.GetComponent<Camera>().backgroundColor = Color.cyan;
         }
     }
 }
