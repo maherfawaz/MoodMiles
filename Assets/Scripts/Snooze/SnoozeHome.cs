d0009099887b5c4a746aa444c440bf6b45f43419
Maher Fawaz <maherfawaz@outlook.com>
2025-06-05 14:48:02 -0400

    Fixed background of snooze home

--- Assets/Scripts/Snooze/SnoozeHome.cs
+++ Assets/Scripts/Snooze/SnoozeHome.cs
@@ -21,7 +21,7 @@ public class SnoozeHome : MonoBehaviour
             mis.SetActive(true);
             prog.SetActive(false);
             count.SetActive(false);
-            main.GetComponent<Camera>().backgroundColor = Color.cyan;
+            main.GetComponent<Camera>().backgroundColor = new Color(178f / 255f, 208f / 255f, 255f / 255f);
         }
         if (Snooze.progress == true)
         {
@@ -32,7 +32,7 @@ public class SnoozeHome : MonoBehaviour
         }
         else
         {
-            main.GetComponent<Camera>().backgroundColor = Color.cyan;
+            main.GetComponent<Camera>().backgroundColor = new Color(178f / 255f, 208f / 255f, 255f / 255f);
         }
     }
 }
