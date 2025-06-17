9481a106aa5a35fbca2bf4f6c8222e48934831c6
Maher Fawaz <maherfawaz@outlook.com>
2025-06-17 15:41:42 -0400

    Update Rewards.cs

--- Assets/Scripts/Rewards.cs
+++ Assets/Scripts/Rewards.cs
@@ -26,6 +26,29 @@ public class Rewards : MonoBehaviour
         }
         if (StaticHp.totalHP <= 0)
         {
+            Snooze.mission = false;
+            Snooze.progress = false;
+            Snooze.attack = false;
+            Snooze.finish = false;
+            Dashie.mission = false;
+            Dashie.progress = false;
+            Dashie.attack = false;
+            Dashie.finish = false;
+            Bruno.mission = false;
+            Bruno.progress = false;
+            Bruno.attack = false;
+            Bruno.finish = false;
+            Zippy.mission = false;
+            Zippy.progress = false;
+            Zippy.attack = false;
+            Zippy.finish = false;
+            NewStepCounter.lastStepOffset = 0;
+            NewStepCounter.lastStepsTaken = 0;
+            Calories.lastCaloriesBurned = 0;
+            Calories.caloriesBurned = 0;
+            Sleep.timeRemaining = 0;
+            StaticHp.totalHP = 4;
+            PlayGamesManager.Instance.SaveData();
             SceneManager.LoadScene("Jail Cutsceen");
         }
     }
